d24cb18b33b438875848fed75d3c0b94300c59a9
Ulukbec <ulukbek691@gmail.com>
2022-10-24 19:25:36 +0600

    commit

---  imp.py
+++  imp.py
@@ -7,7 +7,8 @@ from envparse import env
 import envparse
 
 env.read_envfile('options.env')
-tcolors.cprint(tcolors.Color.BRIGHT_BLUE + colorama.Back.BLACK + os.environ.get('USER_NAME'), os.environ.get('PASSWORD'))
+tcolors.cprint(tcolors.Color.BRIGHT_BLUE + colorama.Back.BLACK + os.environ.get('USER_NAME'))
+tcolors.cprint(tcolors.Color.BRIGHT_BLUE + colorama.Back.BLACK + os.environ.get('PASSWORD'))
 # print(os.environ.get('PASSWORD'))
 
 
